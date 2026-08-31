9385bee73a527eb4f70af96d530874a9de747c84
Giovanni Natanael Peceguini Tonello <gtonello@prodam.sp.gov.br>
2026-08-31 10:44:32 -0300

    teste

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const nomeDoBanco = "1234567897";
-
-function mostrarNomeDoBanco() {
-    console.log(nomeDoBanco);
-}
-mostrarNomeDoBanco();   
+//montar um código onde o codereview vai acusar
+// Código com problemas de legibilidade e boas práticas
+function calc(a,b){return a+b;}var x=10;var y=20;var result=calc(x,y);console.log(result);
